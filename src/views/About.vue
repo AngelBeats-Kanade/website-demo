64fbb0d37125841eeaa29d6b3e1845903cf635c3
AngelBeats <angelbeats177@gmail.com>
2020-09-30 16:12:52 +0800

    add font-awesome

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>I`m an undergrade in UESTC. I have studied in ESE for two years. I am good at C/C++, just know a little about Java. Howerver, I have the energy to learn.</h2>
   </div>
 </template>
---
+++ src/views/Contact.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="contact">
+    <h1>
+      Contact me with
+      <li>
+        <a href="" target="_blank" rel="noopener"
+          ><span class="social fa fa-envelope-o"></span>my e-mail.</a
+        >
+      </li>
+      <li>
+        <a href="" target="_blank" rel="noopener"
+          ><span class="social fa fa-github"></span> Github</a
+        >
+      </li>
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+h3 {
+  margin: 25px 0 0;
+}
+a {
+  color: #42b983;
+}
+@import "//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+</style>
