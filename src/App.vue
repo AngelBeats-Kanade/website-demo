d901fac3ac8885133ea27ccffa2cfe16bc0413a6
AngelBeats-Kanade <angelbeats177@gamil.com>
2021-12-14 20:28:36 +0800

    Delete unuseful style

--- src/App.vue
+++ src/App.vue
@@ -92,16 +92,9 @@ export default defineComponent({
 </script>
 
 <style>
-#components-layout-demo-side .logo {
+.logo {
   height: 32px;
   margin: 16px;
   background: rgba(255, 255, 255, 0.3);
-}
-
-.site-layout .site-layout-background {
-  background: #fff;
-}
-[data-theme='dark'] .site-layout .site-layout-background {
-  background: #141414;
 }
 </style>
