7ef071c0b18f70b34680455079acbf01afee4c10
AngelBeats-Kanade <angelbeats177@gamil.com>
2021-12-14 20:41:08 +0800

    Update code style

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <a-layout style="min-height: 100vh">
     <a-layout-sider v-model:collapsed="collapsed" collapsible>
-      <div class="logo" />
-      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+      <div class="logo"/>
+      <a-menu v-model:selectedKeys="selectedKeys" mode="inline" theme="dark">
         <a-menu-item key="1">
-          <pie-chart-outlined />
+          <pie-chart-outlined/>
           <span>Option 1</span>
         </a-menu-item>
         <a-menu-item key="2">
-          <desktop-outlined />
+          <desktop-outlined/>
           <span>Option 2</span>
         </a-menu-item>
         <a-sub-menu key="sub1">
           <template #title>
             <span>
-              <user-outlined />
+              <user-outlined/>
               <span>User</span>
             </span>
           </template>
@@ -25,7 +25,7 @@
         <a-sub-menu key="sub2">
           <template #title>
             <span>
-              <team-outlined />
+              <team-outlined/>
               <span>Team</span>
             </span>
           </template>
@@ -33,13 +33,13 @@
           <a-menu-item key="8">Team 2</a-menu-item>
         </a-sub-menu>
         <a-menu-item key="9">
-          <file-outlined />
+          <file-outlined/>
           <span>File</span>
         </a-menu-item>
       </a-menu>
     </a-layout-sider>
     <a-layout>
-      <a-layout-header style="background: #fff; padding: 0" />
+      <a-layout-header style="background: #fff; padding: 0"/>
       <a-layout-content style="margin: 0 16px">
         <a-breadcrumb style="margin: 16px 0">
           <a-breadcrumb-item>User</a-breadcrumb-item>
@@ -58,9 +58,9 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import { defineComponent, ref } from 'vue'
-import { PieChartOutlined, DesktopOutlined, UserOutlined, TeamOutlined, FileOutlined } from '@ant-design/icons-vue'
-import { Breadcrumb, Layout, Menu } from "ant-design-vue";
+import {defineComponent, ref} from 'vue'
+import {DesktopOutlined, FileOutlined, PieChartOutlined, TeamOutlined, UserOutlined} from '@ant-design/icons-vue'
+import {Breadcrumb, Layout, Menu} from "ant-design-vue";
 
 export default defineComponent({
   name: 'App',
