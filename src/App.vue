364612b9f3aa32c43e44a57d1c78cd5c3cf06665
AngelBeats <angelbeats177@gmail.com>
2022-09-13 11:20:02 +0800

    Update package version
    Signed-off-by: AngelBeats-Kanade <angelbeats177@gamil.com>

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <v-app>
+    <v-main>
+      <div id="nav">
+        <router-link to="/">Home</router-link>
+        |
+        <router-link to="/about">About</router-link>
+      </div>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="less">
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'App',
+
+  data() {
+    return {
+      //
+    }
+  },
+})
+</script>
+
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -18,14 +36,14 @@
 
 #nav {
   padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
